b56da6c7de1209b6cdd7c9918039ab8e219dffaf
lukelee <lukelee01@gmail.com>
2020-11-13 21:57:28 -0500

    remove typescript folder

--- typescript/typescript-template/bin/typescript-template.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import cdk from "@aws-cdk/core";
-import { TypescriptTemplateStack } from "../lib/typescript-template-stack";
-
-const app = new cdk.App();
-new TypescriptTemplateStack(app, "TypescriptTemplateStack");
--- typescript/typescript-template/lib/typescript-template-stack.ts
+++
@@ -1,7 +0,0 @@
-import * as cdk from "@aws-cdk/core";
-
-export class TypescriptTemplateStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-  }
-}
