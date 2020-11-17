d0e61626ea63c88cc5c71bf61bd15f807cb6b15e
lukelee <lukelee01@gmail.com>
2020-11-17 07:07:19 -0500

    remove unused lines in api_gateway-lambda-authorizer-stack.ts

--- apiGateway-lambda-authorizer/lib/api_gateway-lambda-authorizer-stack.ts
+++ apiGateway-lambda-authorizer/lib/api_gateway-lambda-authorizer-stack.ts
@@ -1,7 +1,6 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as apigateway from "@aws-cdk/aws-apigateway";
-import * as cloudwatch from "@aws-cdk/aws-logs";
 
 export class ApiGatewayLambdaAuthorizerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
