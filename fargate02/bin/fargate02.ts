4d77c7ee300ad5d955066c28311b6568ac52a08f
lukelee <lukelee01@gmail.com>
2020-11-29 20:02:51 -0500

    create fargate02

--- fargate02/bin/fargate02.ts
+++ fargate02/bin/fargate02.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { Fargate02Stack } from '../lib/fargate02-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { Fargate02Stack } from "../lib/fargate02-stack";
+import { FargateJenkinsStack } from "../lib/fargate-jenkins-stack";
 
 const app = new cdk.App();
-new Fargate02Stack(app, 'Fargate02Stack');
+new Fargate02Stack(app, "Fargate02Stack");
+new FargateJenkinsStack(app, "FargateJenkinsStack");
