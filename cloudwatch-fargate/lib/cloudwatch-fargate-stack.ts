7478594be503eb2684357c80f25f822580fcdd59
lukelee <lukelee01@gmail.com>
2020-11-29 22:06:23 -0500

    update gitignore

--- cloudwatch-fargate/lib/cloudwatch-fargate-stack.ts
+++ cloudwatch-fargate/lib/cloudwatch-fargate-stack.ts
@@ -1,9 +1,36 @@
-import * as cdk from '@aws-cdk/core';
+import { Ec2Service } from "@aws-cdk/aws-ecs";
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as cloudwatch from "@aws-cdk/aws-cloudwatch";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import * as schedule from "@aws-cdk/aws-applicationautoscaling";
 
 export class CloudwatchFargateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // vpc
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 2,
+    });
+
+    // cluster
+    const cluster = new ecs.Cluster(this, "Cluster", {
+      vpc,
+    });
+
+    // // deploy batch processing container task in fargate with cloudwatch event schedule
+    // const task = new ecs_patterns.ScheduledFargateTask(this, "Task", {
+    //   cluster,
+    //   scheduledFargateTaskImageOptions: {
+    //     image: ecs.ContainerImage.fromRegistry(
+    //       "mcr.microsoft.com/dotnet/samples:aspnetapp"
+    //     ),
+    //     cpu: 256,
+    //     memoryLimitMiB: 512,
+    //   },
+    //   schedule: new schedule.Schedule()
+    // });
   }
 }
