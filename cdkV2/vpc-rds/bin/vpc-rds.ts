da14f4ab2c0e097400f994211027c6c1e6714b3c
lukelee <lukelee01@gmail.com>
2022-11-24 13:26:19 -0500

    reduce env duplication from vpc-rds

--- cdkV2/vpc-rds/bin/vpc-rds.ts
+++ cdkV2/vpc-rds/bin/vpc-rds.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { vpcStackName } from "./../utility/envUtility";
+import { vpcStackName } from "../utility/envUtility";
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { VpcStack } from "../lib/vpc-stack";
@@ -8,13 +8,15 @@ import { RdsStack } from "../lib/rds-stack";
 import { getEnv } from "../utility/envUtility";
 
 const app = new cdk.App();
+const env = getEnv();
+
 new VpcStack(app, vpcStackName, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  env: getEnv(),
+  env,
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
@@ -30,6 +32,6 @@ new RdsStack(app, "RdsStack", {
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: getEnv(),
+  env,
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
